faa65ba9fdedc81ca50f179f540984d324c14fa2
rafaelspanholi <rafaelles@gmail.com>
2023-03-15 15:51:04 -0300

    Refactor : Button now on 'Button.tsx

--- button-react-app/src/App.tsx
+++ button-react-app/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import Button from './Button'
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <div>
+        
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
@@ -17,9 +19,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <Button setCount={setCount} count={count} />
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
