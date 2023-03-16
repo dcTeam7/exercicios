69378e9a91b4c51d38c4f8e4f2c4298e77d70622
Joel Lacerda <joellacerdaol@gmail.com>
2023-03-16 08:52:30 -0300

    fix: formatar componente Button

--- button-react-app/src/Button.tsx
+++ button-react-app/src/Button.tsx
@@ -3,5 +3,9 @@ import { useState } from "react"
 export default function Button(props: { count: number }) {
   const [count, setCount] = useState(props.count);
 
-  return <button onClick={() => setCount(count + 1)}>count is {count}</button>;
+  return (
+    <button onClick={() => setCount(count + 1)}>
+      count is {count}
+    </button>
+  )
 }
