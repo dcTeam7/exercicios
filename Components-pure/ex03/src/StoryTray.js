754f1b5c748c67e810eb3252a7dd10358d8e4814
kevin <kevindiegodasilvasousa@gmail.com>
2023-03-23 16:45:14 -0300

    feat: init ex03

---
+++ Components-pure/ex03/src/StoryTray.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function StoryTray({ stories }) {
+  stories.push({
+    id: 'create',
+    label: 'Create Story'
+  });
+
+  return (
+    <ul>
+      {stories.map(story => (
+        <li key={story.id}>
+          {story.label}
+        </li>
+      ))}
+    </ul>
+  );
+}
