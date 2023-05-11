54ffb2ba278bb48e29847f76345a759abb5d935a
Hermenegildo Júnior <112488985+JuniorGildo@users.noreply.github.com>
2023-05-11 14:02:46 +0000

    Aula_CriandoServidor_API

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,27 @@
 import express from 'express';
+import cors from 'cors';
+import fs  from 'fs';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 
 const app = express()
 
+app.use(cors())
 // fetch('http://localhost?abraao')
 app.get('/', function (req, res) {
   res.send('Hello World: ' )
+})
+const filename = __dirname + '/index.html';
+app.get('/toligado', function (req, res) {
+
+const fileContents = fs.readFileSync(filename).toString();
+
+console.log(fileContents);
+
+  res.send(fileContents )
 })
 
 const PORT = 9000;
